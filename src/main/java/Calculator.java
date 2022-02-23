dc3467fc3ef1125392a72a97eebc00a06136cfb3
mapioca <77710392+mapioca@users.noreply.github.com>
2022-02-22 22:45:46 -0700

    Implemented createUniqueID function

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.UUID;
 
 class Calculator {
 
@@ -67,8 +68,8 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-
-        return null;
+        UUID uuid = UUID.randomUUID();
+        return n + uuid;
     }
 
 
